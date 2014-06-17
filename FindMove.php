1d833a9fbd9c6cd4a4079676230f2b69c8bfc6f0
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2014-06-16 23:40:51 -0400

    Almost there

--- FindMove.php
+++ FindMove.php
@@ -1,9 +1,10 @@
 <?php
-define("MAX_DEPTH", 7);//Takes about 1.5-2 seconds
+define("MAX_DEPTH", 5);//Takes about 1.5-2 seconds
 class FindMove{
 	private $a, $b;
 	private $bestMove = NULL;//integer 0-6
-	private $ref = array();//Map between column and current index of space (so if column i is empty, ref[i] = 6
+	private $ref = array();//Map between column and current index of top SPACE (so if column i is empty, ref[i] = 6
+	private $score = 0;
 
 	public function __construct($a){
 		$this->a = $a;
@@ -19,7 +20,7 @@ class FindMove{
 	}
 
 	private function min(&$board, $depth){
-		if($depth >= $this->MAX_DEPTH){
+		if($depth >= MAX_DEPTH){
 			return $this->score($board);
 		}
 		$min = NULL;
@@ -47,7 +48,7 @@ class FindMove{
 	//board must be passed by reference, apparently PHP copies arrays by default :/
 	//Goes through all player a's options, returns the maximum value (of the mins of those options)
 	private function max(&$board, $depth){
-		if($depth >= $this->MAX_DEPTH){
+		if($depth >= MAX_DEPTH){
 			return $this->score($board);
 		}
 		$max = NULL;
@@ -59,6 +60,8 @@ class FindMove{
 			//apply move, calculate the min for this state
 			$this->doMove($board, $i, $this->a);
 			$val = $this->min($board, $depth+1);
+			//printf("move %d, score %d\n", $i, $val);
+			//$this->printBoard($board);
 			$this->undoMove($board, $i, $this->a);
 			if($max == NULL || $val > $max){
 				$max = $val;
@@ -89,33 +92,148 @@ class FindMove{
 		$board[$this->ref[$i]+1][$i] = 0;
 		$this->ref[$i] = $this->ref[$i] + 1;
 	}
-	//Return a score from a x's perspective
-	public function score(&$board){
-		//count number of possible ways to win, place very high value on 2 possibilities to win in 1 move, place highest value on winning
+	private function scoreDiff(&$board, $i, $j, $player){
+
+	}
+	private function playerWeight($player){
+		return $player == $this->a ? 1 : -1;
+	}
+	//given a step through the i and j, count consecutive lines of pieces and return a score
+	private function scorePaths(&$board, $iStart, $jStart, $iStep, $jStep){
 		$score = 0;
-		//go down each column, count number of longest path at top
-		for($j = 0; $j < 7; $j++){
-			if($this->ref[$j] >= 0){
-				$pathLength = 0;
-				for($i = $this->ref[$j], $i >= 0; $i--){
-					if($board[$i][$j] == $this->a){
-						$pathLength++;
+		//go through each row, count # of possible wins
+		$lspaces = 0;
+		$rspaces = 0;
+		$curPlayer = -1;
+		$curPlayerCount = 0;
+		$end = false;
+		$i = $iStart;
+		$j = $jStart;
+		while($i < 7 && $j < 7 && $i >= 0 && $j >= 0){
+			//printf("i%d,j%d,is%d,js%d\n", $i,$j, $iStep,$jStep);
+			if($board[$i][$j] == 0){
+				if($curPlayer != -1){
+					//end of path
+					$end = true;
+				}
+				else{
+					$lspaces++;
+				}
+			}
+			else{
+				if($curPlayer != -1){
+					if($curPlayer != $board[$i][$j]){
+						//end of path
+						$end = true;
+					}
+					else{
+						//continuation of path
+						$curPlayerCount++;
+					}
+				}
+				else{
+					//first path
+					$curPlayer = $board[$i][$j];
+					$curPlayerCount++;
+				}
+			}
+
+			if($end){
+				//count r spaces
+				$ip = $i;
+				$jp = $j;//i prime, j prime
+				while($ip < 7 && $jp < 7 && $ip >= 0 && $jp >= 0){
+					if($board[$ip][$jp] == 0){
+						$rspaces++;
 					}
 					else{
 						break;
 					}
+					$ip += $iStep;
+					$jp += $jStep;
 				}
-				if(4 - $pathLength < (7 - $this->ref[$i])){
+				if($rspaces + $lspaces + $curPlayerCount >= 4){
+					//printf("Row : %d, Col: %d, Path : %d, Player: %d\n", $i, $j, $curPlayerCount, $curPlayer);
+					//possible win
+					if($curPlayerCount >= 4){
+						//winning move
+						$score += 1000 * $this->playerWeight($curPlayer);
+					}
+					else{
+						$score +=  $curPlayerCount * $this->playerWeight($curPlayer);
+					}
+				}
+				$curPlayerCount = 0;
+				$curPlayer = -1;
+				$lspaces = 0;//reset path
+				$rspaces = 0;
+				$end = false;
+				$j -= $jStep;//want to continue on the piece we skipped
+				$i -= $iStep;
+			}
+			$i += $iStep;
+			$j += $jStep;
+		}
+		if($curPlayerCount >= 4){
+			//if player wins without any spaces after, need to update score
+			$score += 1000 * $this->playerWeight($curPlayer);
+		}
+		return $score;
+
+	}
+	//Return a score from a x's perspective
+	public function score(&$board){
+		//count number of possible ways to win, place very high value on 2 possibilities to win in 1 move, place highest value on winning
+		$score = 0;
+		$curPlayer = -1;
+		//go down each column, count number of longest path at top
+		for($j = 0; $j < 7; $j++){
+			if($this->ref[$j] >= -1 && $this->ref[$j] < 6){
+				$pathLength = 0;
+				$curPlayer = $board[$this->ref[$j]+1][$j];
+				for($i = $this->ref[$j]+1; $i < 7; $i++){
+					if($board[$i][$j] != $curPlayer){
+						break;
+					}
+					else{
+						$pathLength++;
+					}
+				}
+				if(4 - $pathLength < $this->ref[$j] + 1){
 					//if # needed is less than # left, no help to us
 				}
 				else{
-					$score += $pathLengh;//longer path is better, scale this by a weight (possibly by using ml)
+					if($pathLength >= 4){
+						//this player won
+						$score += 1000 * $this->playerWeight($curPlayer);
+					}
+					else{
+						//player hasn't won, but it's possible
+						$score += $pathLength * $this->playerWeight($curPlayer);//longer path is better, scale this by a weight (possibly by using ml)
+					}
 				}
 			}
 		}
-		//go through each row, count # of possible wins
+		//go through rows
+		for($i = 0; $i < 7; $i++){
+			//$score += $this->scorePaths($board, $i, 0, 0, 1);
+		}
+		//go through the diagonals
+		for($i = 3; $i < 7; $i++){
+			$score += $this->scorePaths($board, $i, 0, -1, 1);
+		}
+		for($j = 1; $j <= 3; $j++){
+			$score += $this->scorePaths($board, 6, $j, -1, 1);
+		}
 
-		return rand();//should be fine, it'll just always select the first move for now
+		for($i = 0; $i <= 3; $i++){
+			$score += $this->scorePaths($board, $i, 0, 1, 1);
+		}
+		for($j = 1; $j <= 3; $j++){
+			$score += $this->scorePaths($board, 0, $j, 1, 1);
+		}
+
+		return $score;//should be fine, it'll just always select the first move for now
 	}
 	public function findMove(&$board){
 		if(sizeof($board) != 7 || sizeof($board[0]) != 7){
@@ -151,10 +269,11 @@ $ex = array(
 	array(0, 0, 0, 0, 0, 0, 0),
 	array(0, 0, 0, 0, 0, 0, 0),
 	array(0, 0, 0, 0, 0, 0, 0),
-	array(0, 0, 2, 1, 0, 0, 0),
-	array(0, 1, 1, 2, 2, 0, 0)
-	);
-$mover = new FindMove(1);
+	array(0, 0, 0, 1, 0, 0, 0),
+	array(0, 1, 1, 2, 2, 0, 2)
+);
+$mover = new FindMove(2);
+//printf("Score is %d\n" , $mover->score($ex));
 $move = $mover->findMove($ex);
 printf("Best move is %d\n", $move);
 $end = microtime(true);
--- README.md
+++ README.md
@@ -1,6 +1,13 @@
+Finished
+========
+- Optimized space by passing array as reference.
+- Optimized by caching space location in an array so additional array traversal is not needed when doing/undoing moves
 
-Optimized space by passing array as reference.
-Optimized by caching space location in an array so additional array traversal is not needed when doing/undoing moves.
+Now
+===
+- Scoring does not take into account skips, e.g. 1 1 0 1, that zero is the best move for 1
 
-Eventually:
+Eventually
+==========
 - Optimize further by using a single dimensional array (or by flipping columns and rows so cache hits are more probable)
+- Optimize scoring by placing depth limit in loop and only calculating score difference and updating
