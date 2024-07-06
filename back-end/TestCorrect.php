56d8c4c8b12fed898b133cab9387b6d2f0aa0727
Kevin Albertson <kevin.eric.albertson@gmail.com>
2024-07-05 21:27:41 -0400

    update TestCorrect.php

--- back-end/TestCorrect.php
+++ back-end/TestCorrect.php
@@ -1,30 +1,57 @@
 <?php
+// Run test with: `php -f TestCorrect.php.
 require_once("C4AI.php");
-class TestCorrect extends PHPUnit_Framework_TestCase{
-	public function testPruning(){
-		//play an entire game against itself, ensure that the move with/without the pruning matches
-		$board = array(
-			array(0, 0, 0, 0, 0, 0, 0),
-			array(0, 0, 0, 0, 0, 0, 0),
-			array(0, 0, 0, 0, 0, 0, 0),
-			array(0, 0, 0, 0, 0, 0, 0),
-			array(0, 0, 0, 0, 0, 0, 0),
-			array(0, 0, 0, 0, 0, 0, 0),
-			array(0, 0, 0, 0, 0, 0, 0)
-		);
-		$ai = C4AI::getInstance();
-		for($i = 0; $i < 4; $i++){
-			$player = 2;
-			if($i % 2 == 0){
-				$player = 1;
-			}
-			$pruneMove = $ai->findMove($board, $player, true);
-			$noPruneMove = $ai->findMove($board, $player, false);
-			$this->assertEquals($pruneMove, $noPruneMove);
-			$ai->doMove($board, $pruneMove, $player);
-			printf("Player %d move to %d\n", $player, $pruneMove);
-			$ai->printBoard($board);
+
+function testWorks(){
+	// Test playing an entire game.
+	$board = array(
+		array(0, 0, 0, 0, 0, 0, 0),
+		array(0, 0, 0, 0, 0, 0, 0),
+		array(0, 0, 0, 0, 0, 0, 0),
+		array(0, 0, 0, 0, 0, 0, 0),
+		array(0, 0, 0, 0, 0, 0, 0),
+		array(0, 0, 0, 0, 0, 0, 0),
+		array(0, 0, 0, 0, 0, 0, 0)
+	);
+	$ai = C4AI::getInstance();
+	for ($moveIter = 0; $moveIter < 100; $moveIter++) {
+		$player = 2;
+		if($moveIter % 2 == 0){
+			$player = 1;
 		}
+		$moves = $ai->findAllMoves($board, $player);
+		# Pick the highest score move.
+		$bestMove = -1;
+		$bestScore = -1 * INF;
+		foreach ($moves as $move => $score) {
+			if ($score > $bestScore) {
+				$bestMove = $move;
+				$bestScore = $score;
+			}
+		}
+
+		// Play the highest score move.
+		$ai->doMove($board, $bestMove, $player);
+		
+		// Check if the board is won.
+		$moveJ = $bestMove;
+		$moveI = 0;
+		for ($i = 0; $i < 7; $i++) {
+			if ($board[$i][$moveJ] != 0) {
+				$moveI = $i;
+				break;
+			}
+		}
+		if ($ai->hasWon($board, $moveI, $moveJ, $player)) {
+			printf ("Player %d has won:\n", $player);
+			$ai->printBoard($board);
+			break;
+		}
+		
+		printf("Player %d moved to %d\n", $player, 0);
+		$ai->printBoard($board);
 	}
 }
 
+testWorks();
+printf ("Tests passed");
